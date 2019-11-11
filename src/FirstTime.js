b733290f515f424d6e921e12bd37340300ec4730
Bryan Ollendyke <bto108@psu.edu>
2019-11-11 11:34:59 -0500

    some minor feedback / tweaks

--- src/FirstTime.js
+++ src/FirstTime.js
@@ -2,9 +2,11 @@ import { html, css, LitElement } from 'lit-element';
 
 export class FirstTime extends LitElement {
   static get styles() {
-    return css`
+    // this being an array will help setup for the future when there's the ability to import
+    // the usa style sheet into this element (and others) for easier use
+    return [css`
       :host {
-        --first-time-text-color: #000;
+        --first-time-text-color: #000; /* is this something that can be changed as per the usa standard? */
         display: block;
         padding: 25px;
         color: var(--first-time-text-color);
@@ -12,13 +14,14 @@ export class FirstTime extends LitElement {
         font-family: Source Sans Pro Web,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
         font-size: 1.06rem;
         line-height: 1.5;
-        background-position: 1.75rem 1.25rem;
+        background-position: 1.75rem 1.25rem; /* This might be ok but REM can operate a bit odd in shadow roots at times */
         background-repeat: no-repeat;
         background-size: 2rem;
         padding: 1.25rem 1.25rem 1rem 1.75rem;
         position: relative;
       }
-
+      /* the usa-alert__heading class scoping isn't needed in here unless you want to be keeping with the existing style names */
+      /* otherwise, you could just do h3 by itself without issue */
       h3.usa-alert__heading{
         font-family: Source Sans Pro Web,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
         font-size: 1.33rem;
@@ -26,39 +29,35 @@ export class FirstTime extends LitElement {
         margin-top: 0;
         margin-bottom: .5rem;
       }
-
+      /* same as above, dono if this scoping is needed but it's not bad to do */
       .usa-alert__body{
         padding-left: 3.25rem;
       }
 
       :host([status='success']){
         background-image: url(https://designsystem.digital.gov/assets/img/alerts/success.svg),linear-gradient(transparent,transparent);
-        background-repeat: no-repeat;
         background-color: #ecf3ec;
         border-left: solid #00a91c 10px;
       }
 
       :host([status='warning']){
         background-image: url(https://designsystem.digital.gov/assets/img/alerts/warning.svg),linear-gradient(transparent,transparent);
-        background-repeat: no-repeat;
         background-color: #faf3d1;
         border-left: solid #ffbe2e 10px;
       }
 
       :host([status='error']){
         background-image: url(https://designsystem.digital.gov/assets/img/alerts/error.svg),linear-gradient(transparent,transparent);
-        background-repeat: no-repeat;
         background-color:#f4e3db;
         border-left: solid #d63e04 10px;
       }
 
       :host([status='info']){
         background-image: url(https://designsystem.digital.gov/assets/img/alerts/info.svg),linear-gradient(transparent,transparent);
-        background-repeat: no-repeat;
         background-color: #e7f6f8;
         border-left: solid #00bde3 10px;
       }
-    `;
+    `];
   }
 
   static get properties() {
